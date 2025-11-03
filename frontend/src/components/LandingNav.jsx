e04b4de7c4037dc626cd0295bfd4b8c5eea3d098
VISHWARAJ-G <rajgvishwa630@gmail.com>
2025-11-03 14:25:19 +0530

    Landing page done

--- frontend/src/components/LandingNav.jsx
+++ frontend/src/components/LandingNav.jsx
@@ -26,9 +26,9 @@ function LandingNav() {
           className="custom-rays"
         />
 
-        <nav className="absolute top-0 left-0 w-full flex justify-between items-center py-4 px-10 z-10 text-white">
+        <nav className="absolute top-0 left-0 w-full flex justify-between items-center py-10 px-10 z-10 text-white">
           <div className="w-[10%]">
-            <img src="Logo.png" alt="" className="h-16 w-auto" />
+            <img src="Logo.png" alt="" className="w-auto" />
           </div>
 
           <div className="flex justify-center items-center gap-10 font-semibold">
@@ -46,15 +46,37 @@ function LandingNav() {
           <div>
             <a
               href="#"
-              className="bg-cyan-500 hover:bg-cyan-400 text-black font-semibold px-5 py-2 rounded-lg transition"
+              className="bg-cyan-400 hover:bg-cyan-500 text-black font-semibold px-5 py-2 rounded-lg transition"
             >
               Dashboard
             </a>
           </div>
         </nav>
-        <div className="flex items-center justify-center">
-          <div className="text-white absolute top-48 flex w-[80%] text-center text-7xl font-bold">
+        <div className="flex flex-col items-center justify-center top-48 absolute ">
+          <div className="text-white flex w-[80%] text-center text-7xl font-bold">
             <h1>Create a Portfolio website that stands out</h1>
+          </div>
+          <div className="text-white mt-10 text-center flex w-[100%] justify-center items-center text-lg font-bold">
+            <h2 className="w-[50%] text-center">
+              Showcase your work and attract clients. Build and promote your
+              online portfolio using powerful AI, design and business tools
+            </h2>
+          </div>
+          <div className="pt-10">
+            <a
+              href="#"
+              className="bg-cyan-500 rounded-xl hover:bg-cyan-400 font-bold px-10 py-4 text-white transition w-full text-2xl"
+            >
+              Get Started
+            </a>
+          </div>
+          <div className="w-full mt-12 flex items-center px-40 justify-center tracking-wider">
+            <div className="text-5xl font-bold">Start Now!</div>
+            {/* <div>
+              <button className="bg-cyan-500 p-4 text-black rounded-xl font-bold">
+                About US
+              </button>
+            </div> */}
           </div>
         </div>
       </div>
